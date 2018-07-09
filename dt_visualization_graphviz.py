9aa9b225716eec487c7e338e5e8d32602f58b901
Siddhesh Chavan <32998144+siddesh001@users.noreply.github.com>
2018-07-09 17:06:04 +0530

    Update the code for building the decision tree

--- dt_visualization_graphviz.py
+++ dt_visualization_graphviz.py
@@ -1,3 +1,18 @@
+from sklearn import datasets
+iris=datasets.load_iris()
+from sklearn.tree import DecisionTreeClassifier
+import pandas as pd
+#----------------------------------------------------------------------
+#Code to create an object of decision tree and building a decision tree.
+#----------------------------------------------------------------------
+df1=pd.DataFrame(iris.data,columns=iris.feature_names)
+y=iris.target
+dtree=DecisionTreeClassifier()
+dtree.fit(df1,y)
+
+#----------------------------------------------------------------------
+#Code for visualization of the decision tree in human readable format
+#----------------------------------------------------------------------
 from sklearn.externals.six import StringIO  
 from IPython.display import Image  
 from sklearn.tree import export_graphviz
